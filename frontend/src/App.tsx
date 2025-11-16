c1b525b4f34d4c33b7e44b9c37284a7b945259d6
in7570 <i7570@naver.com>
2025-11-17 03:02:08 +0900

    chore: remove Vite default template and initialize project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,16 @@
-import { useEffect } from "react";
-import "./App.css";
-
+import { useEffect } from 'react';
+import './App.css';
 
 function App() {
   useEffect(() => {
-    fetch("http://localhost:3000/game/state")
+    fetch('http://localhost:3000/game/state')
       .then((res) => res.json())
       .then((data) => {
-        console.log("현재 보드 상태:", data);
+        console.log('현재 보드 상태:', data);
       });
   }, []);
 
-  return (
-    <>
-     
-    </>
-  );
+  return <></>;
 }
 
 export default App;
