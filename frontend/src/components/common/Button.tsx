769eec6bae3be9b7b118e18ada66aaa5ed20172c
in7570 <i7570@naver.com>
2025-11-18 04:56:49 +0900

    feat: add Button component

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,3 +1,16 @@
-export const Button = () => {
-  return <div>Button</div>;
+import './Button.css';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+export const Button = ({ children, onClick }: ButtonProps) => {
+  return (
+    <div className='button-container'>
+      <button className='button' onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
 };
