5846d878f1e74847f351ff7450ac155b0010c15c
in7570 <i7570@naver.com>
2025-11-18 04:58:52 +0900

    feat: add Piece component with team-based styling

--- frontend/src/components/pieces/Piece.tsx
+++ frontend/src/components/pieces/Piece.tsx
@@ -1,3 +1,35 @@
-export const Piece = () => {
-  return <div>Piece</div>;
+import { FONTS } from '../../constants/fonts';
+import type { PieceType, Team } from '../../types/types';
+import { pieceConfig } from './pieceConfig';
+import './Piece.css';
+
+type PieceProps = {
+  type: PieceType;
+  team: Team;
+  size?: number; // 크기를 숫자로 받기 (기본값: 46px)
+};
+
+export const Piece = ({ type, team, size = 46 }: PieceProps) => {
+  const innerSize = size * 0.87; // 테두리를 위해 안쪽 크기를 약간 작게
+
+  return (
+    <div className='piece-container'>
+      <div
+        className={`piece-border ${team}`}
+        style={{ width: size, height: size }}
+      >
+        <div
+          className={`piece-octagon ${team}`}
+          style={{
+            width: innerSize,
+            height: innerSize,
+            fontFamily: FONTS.piece[team],
+            fontSize: size * 0.45,
+          }}
+        >
+          {pieceConfig[type][team]}
+        </div>
+      </div>
+    </div>
+  );
 };
